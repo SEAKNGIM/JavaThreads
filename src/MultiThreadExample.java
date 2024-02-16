133ad803774a8180e2d0a64b21b5c386e38f2950
SEAKNGIM <seakngimsr37@gmail.com>
2024-02-16 21:45:53 +0700

    Using multiple threads, wirte a java program in order to output

--- src/MultiThreadExample.java
+++ src/MultiThreadExample.java
@@ -1,42 +1,83 @@
 class MyThread extends Thread {
-    private String message;
+    private String msgiveup;
+    private String mswelcom;
+    private String msloading;
+    private String asterisk;
+    private String dash;
 
-    public MyThread(String message) {
-        this.message = message;
+    public MyThread(String mswelcom, String asterisk,String msgiveup, String dash, String msloading) {
+        this.msgiveup = msgiveup;
+        this.asterisk = asterisk;
+        this.mswelcom = mswelcom;
+        this.msloading = msloading;
+        this.dash = dash;
     }
-
     @Override
     public void run() {
-        for (int i = 0; i < 5; i++) {
-            System.out.println(message);
+        for (int i = 0; i < mswelcom.length(); i++) {
+            System.out.print(mswelcom.charAt(i) + "");
             try {
                 // Sleep for a random duration to simulate some work
-                Thread.sleep((long) (Math.random() * 1000));
+                Thread.sleep((long) (Math.random() * 500));
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+
+        for (int i = 0; i < 50; i++) {
+            System.out.print(asterisk);
+            try {
+                // Sleep for a random duration to simulate some work
+                Thread.sleep((long) (Math.random() * 500));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        for (int i = 0; i < msgiveup.length(); i++) {
+            System.out.print(msgiveup.charAt(i) + "");
+            try {
+                // Sleep for a random duration to simulate some work
+                Thread.sleep((long) (Math.random() * 500));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        for (int i = 0; i < 50; i++) {
+            System.out.print(dash);
+            try {
+                // Sleep for a random duration to simulate some work
+                Thread.sleep((long) (Math.random() * 500));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        for (int i = 0; i < msloading.length(); i++) {
+            System.out.print(msloading.charAt(i) + "");
+            try {
+                // Sleep for a random duration to simulate some work
+                Thread.sleep((long) (Math.random() * 500));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
 
 public class MultiThreadExample {
     public static void main(String[] args) {
-        // Create two threads with different messages
-        MyThread thread1 = new MyThread("Thread 1 - Hello from Thread!");
-        MyThread thread2 = new MyThread("Thread 2 - Hi from Thread!");
+      // Create threads with different messages
+    MyThread thread = new MyThread("Welcome to CSTAD!\n","*", "\nDon't give up this opportunity, do your best first\n", "-", "\nDownloading..........");
 
-        // Start both threads
-        thread1.start();
-        thread2.start();
+    // Start threads
+        thread.start();
 
-        // Wait for both threads to finish
+    // Wait for threads to finish
         try {
-            thread1.join();
-            thread2.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("Main thread exiting.");
+        thread.join();
+    } catch (InterruptedException e) {
+        e.printStackTrace();
     }
+        System.out.println("completed 100%.");
+}
 }
